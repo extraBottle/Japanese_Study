e896aac3ec5eb7e6a8ebd6801a207344eea53114
extraBottle <tim2425@naver.com>
2025-08-11 01:31:11 +0900

    init

---
+++ script.js
@@ -0,0 +1,45 @@
+const hiragana = [
+    "あ", "い", "う", "え", "お",
+    "か", "き", "く", "け", "こ",
+    "さ", "し", "す", "せ", "そ",
+    "た", "ち", "つ", "て", "と",
+    "な", "に", "ぬ", "ね", "の",
+    "は", "ひ", "ふ", "へ", "ほ"
+]
+const hiraganaKor = [
+    "아", "이", "우", "에", "오",
+    "카", "키", "쿠", "케", "코",
+    "사", "시", "스", "세", "소",
+    "타", "치", "츠", "테", "토",
+    "나", "니", "누", "네", "노",
+    "하", "히", "후", "헤", "호"
+]
+let flag = true;
+
+function randomPick(){
+    if (flag) {
+        flag = !flag;
+        return hiragana[Math.floor(Math.random() * hiragana.length)];
+    }    
+    else {
+        flag = !flag;
+        return hiraganaKor[Math.floor(Math.random() * hiragana.length)];
+    }    
+}
+function findAnswer(){    
+    if (!flag) {
+        const index = hiragana.indexOf(document.getElementById("question-box").innerHTML);
+        return hiraganaKor[index];
+    }    
+    else {
+        const index = hiraganaKor.indexOf(document.getElementById("question-box").innerHTML);
+        return hiragana[index];
+    }    
+}
+function clickNext() {
+  document.getElementById("question-box").innerHTML = randomPick();
+  document.getElementById("answer-text").innerHTML = "";
+}
+function clickAnswer() {
+  document.getElementById("answer-text").innerHTML = findAnswer();
+}
