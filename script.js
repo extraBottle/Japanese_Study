1c0f0e48bdbaf5f23b213dc11bbfa77d615f44c2
extraBottle <tim2425@naver.com>
2025-08-14 16:55:09 +0900

    add 가타카나

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+const japanese = []
+const kor = [
+    "아", "이", "우", "에", "오",
+    "카", "키", "쿠", "케", "코",
+    "사", "시", "스", "세", "소",
+    "타", "치", "츠", "테", "토",
+    "나", "니", "누", "네", "노",
+    "하", "히", "후", "헤", "호",
+    "마", "미", "무", "메", "모",
+    "야",       "유",      "요",
+    "라", "리", "루", "레", "로",
+    "와",             "오(wo)",
+    "응"
+]
 const hiragana = [
     "あ", "い", "う", "え", "お",
     "か", "き", "く", "け", "こ",
@@ -11,45 +25,62 @@ const hiragana = [
     "わ",                 "を",
     "ん"
 ]
-const hiraganaKor = [
-    "아", "이", "우", "에", "오",
-    "카", "키", "쿠", "케", "코",
-    "사", "시", "스", "세", "소",
-    "타", "치", "츠", "테", "토",
-    "나", "니", "누", "네", "노",
-    "하", "히", "후", "헤", "호",
-    "마", "미", "무", "메", "모",
-    "야",       "유",      "요",
-    "라", "리", "루", "레", "로",
-    "와",             "오(wo)",
-    "응"
+const gatakana = [
+    "ア", "イ", "ウ", "エ", "オ",
+    "カ", "キ", "ク", "ケ", "コ",
+    "サ", "シ", "ス", "セ", "ソ",
+    "タ", "チ", "ツ", "テ", "ト",
+    "ナ", "ニ", "ヌ", "ネ", "ノ",
+    "ハ", "ヒ", "フ", "ヘ", "ホ",
+    "マ", "ミ", "ム", "メ", "モ",
+    "ヤ",      "ユ",       "ヨ",
+    "ラ", "リ", "ル", "レ", "ロ",
+    "ワ",                 "ヲ",
+    "ン"
 ]
 let flag = true;
 let counting = 0;
 
 document.addEventListener("DOMContentLoaded", function() {
   document.getElementById("total-count").innerHTML = hiragana.length;
+  japanese.push(...hiragana); 
 });
 
 function randomPick(){
     if (flag) {
         flag = !flag;
-        return hiragana[Math.floor(Math.random() * hiragana.length)];
+        return japanese[Math.floor(Math.random() * hiragana.length)];
     }    
     else {
         flag = !flag;
-        return hiraganaKor[Math.floor(Math.random() * hiragana.length)];
+        return kor[Math.floor(Math.random() * hiragana.length)];
     }    
 }
 function findAnswer(){    
     if (!flag) {
-        const index = hiragana.indexOf(document.getElementById("question-box").innerHTML);
-        return hiraganaKor[index];
+        const index = japanese.indexOf(document.getElementById("question-box").innerHTML);
+        return kor[index];
     }    
     else {
-        const index = hiraganaKor.indexOf(document.getElementById("question-box").innerHTML);
-        return hiragana[index];
+        const index = kor.indexOf(document.getElementById("question-box").innerHTML);
+        return japanese[index];
     }    
+}
+function chooseRange() {
+    const checkedRadio = document.querySelector('input[name="range"]:checked');
+    japanese.length = 0;    
+    counting = 0;
+    document.getElementById("counter").innerHTML = counting;
+    if (checkedRadio.value == "히라가나"){
+        document.getElementById("total-count").innerHTML = hiragana.length;
+        japanese.push(...hiragana);        
+    }
+    else if (checkedRadio.value == "가타카나"){
+        document.getElementById("total-count").innerHTML = gatakana.length;
+        japanese.push(...gatakana);        
+    }
+    document.getElementById("question-box").innerHTML = "문제";
+    document.getElementById("answer-text").innerHTML = "정답";
 }
 function clickNext() {
   document.getElementById("question-box").innerHTML = randomPick();
